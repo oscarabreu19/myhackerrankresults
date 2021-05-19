bcdfaef201747b120cfc43d7b50d6ea4e1b68487
Oscar Galindez <oscarabreu19@gmail.com>
2021-05-19 08:03:57 -0300

    [add][primera_rama] Agrega el primer comentario
    [add][primera_rama] Se agrega un segundo comentario
    
    [edit][primera_rama] Se edita el comentario sin alterar la historia
    
    [edit][primera_rama] Se edita el mensaje del commit
    
    [add][primera_rama] Se elimina un comentario
    
    [add][primera_rama] Se saluda

--- 10-days-of-javascript/app.js
+++ 10-days-of-javascript/app.js
@@ -1,1 +1,2 @@
 // https://www.hackerrank.com/domains/tutorials/10-days-of-javascript
+// hola
--- auxTest/app.js
+++ auxTest/app.js
@@ -1,3 +1,5 @@
+// Agrega un primer comentario
+// Se elimina el segundo comentario de la histora sin agregar un commit nuevo
 function filterNumbersFromArray(arr: any[]): void {
     // Write the code that goes here
     const exRegex = new RegExp(`[[0-9]]`,'gi');
